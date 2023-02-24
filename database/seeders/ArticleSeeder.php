d601b9e1c98edaaa94606c8c40ff1773876c37b0
WojciechRzoska <wojciechrzoska@gmail.com>
2023-02-24 12:31:40 +0100

    add seeders

---
+++ database/seeders/ArticleSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Article;
+use App\Models\Author;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ArticleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $authors = Author::all();
+
+        $articlesCount = [3, 2, 1]; // Number of articles to create for each author
+
+        foreach ($authors as $key => $author) {
+            $count = $articlesCount[$key] ?? 0;
+
+            Article::factory()
+                ->count($count)
+                ->create()
+                ->each(function ($article) use ($author) {
+                    $article->authors()->attach($author);
+                });
+        }
+    }
+}
---
+++ database/seeders/AuthorSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Author;
+
+class AuthorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Author::create(['name' => 'Sławomir Peszko']);
+        Author::create(['name' => 'Robert Lewandowski']);
+        Author::create(['name' => 'Tomasz Hajto']);
+        Author::create(['name' => 'Łukasz Piszczek']);
+        Author::create(['name' => 'Artur Boruc']);
+    }
+}
